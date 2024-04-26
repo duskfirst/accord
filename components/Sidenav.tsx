eb07100ae23d59f46c99f2d33629e9012869956b
Devwardhan-Kothari <88496527+Devwardhan-Kothari@users.noreply.github.com>
2024-04-26 11:22:51 +0530

    Add tooltips to sidenav

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -1,21 +1,44 @@
 "use client";
 import { LogOut, CircleUserRound } from "lucide-react";
 import Link from "next/link";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Sidenav = () => {
     return (
         <div className="flex flex-col w-8 h-full justify-between items-center p-2">
             <div>Logo</div>
             <div className="flex flex-col gap-3 justify-between items-center">
-                <Link href={`/profile`} className="rounded-sm hover:bg-zinc-600">
-                    <CircleUserRound />
-                </Link>
-                <LogOut
-                    className="hover:bg-zinc-600 rounded-sm"
-                    onClick={() => {
-                        alert("Logged out");
-                    }}
-                />
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Link href={`/profile`} className="rounded-sm hover:bg-zinc-600">
+                                <CircleUserRound />
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Profile</p>
+                        </TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <LogOut
+                                className="hover:bg-zinc-600 rounded-sm"
+                                onClick={() => {
+                                    alert("Logged out");
+                                }}
+                            />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Log out</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             </div>
         </div>
     );
