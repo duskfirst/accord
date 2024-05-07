553a65859b941c22fe82d39aa5ffcd67668e5511
Arjit Verma <arjitverma73@gmail.com>
2024-05-08 00:46:40 +0530

    Add home page

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,21 +1,29 @@
 import { Button } from "@/components/ui/button";
 
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../public/Home.png";
 
-
+import "./Home.css";
 const Home = () => {
-    return (
-        <div className="flex w-full items-center justify-center h-full flex-col gap-16">
-            <div className="font-extrabold text-3xl">HOME</div>
-            <div className="gap-20 flex">
-                <Button className="w-32">
-                    <Link href={"/login"} >Login</Link>
-                </Button>
-                <Button className="w-32">
-                    <Link href={"/register"} >Register</Link>
-                </Button>
-            </div>           
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <Image src={logo} alt="Logo" className="App-logo" />
+        <h1 className="App-heading">CHAT APP</h1>{" "}
+        {/* Moved this line below the image */}
+        <div className="App-link">
+          <a>
+            <button className="App-button">LOGIN</button>
+            <p className="App-text">Not registered yet?</p>
+            <button className="App-button">REGISTER</button>
+          </a>
         </div>
-    );
+      </header>
+    </div>
+  );
 };
 export default Home;
