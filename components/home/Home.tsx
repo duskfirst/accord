537a9f5e729a4ca57bbc727190de43837b5052f8
boxlesscat <boxlesscat@gmail.com>
2024-05-08 12:08:24 +0530

    Fix chat background change

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,31 +1,31 @@
-import { Button } from "@/components/ui/button";
-
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../public/Home.png";
+import logo from "@/public/Home.png";
 
 import "./Home.css";
+
+
 const Home = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <Image src={logo} alt="Logo" className="App-logo" />
-        <h1 className="App-heading">CHAT APP</h1>{" "}
-        {/* Moved this line below the image */}
-        <div className="App-link flex flex-col gap-1">
-          <a href="/login">
-            <button className="App-button">LOGIN</button>
-          </a>
-          <p className="App-text">Not registered yet?</p>
-          <a href="/register">
-            <button className="App-button">REGISTER</button>
-          </a>
+    return (
+        <div className="body w-full h-full">
+            <div className="Home">
+                <header className="App-header">
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <Image src={logo} alt="Logo" className="App-logo" />
+                    <h1 className="App-heading uppercase">Accord</h1>
+                    <div className="App-link flex gap-4 text-md">
+                        <Link href="/login" className="hover:underline">
+                            Login
+                        </Link>
+                        <Link href="/register" className="hover:underline">
+                            Register
+                        </Link>
+                    </div>
+                </header>
+            </div>
         </div>
-      </header>
-    </div>
-  );
+    );
 };
 export default Home;
